2bbb38a5c82ef74249e58670dd101997c69212bc
Ötvös Ákos <akika02531@gmail.com>
2025-01-24 09:38:21 +0100

    done

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -12,6 +12,35 @@
         public int ProductCount => _products.Count;
 
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
+        public void AddProduct(string name, double price)
+        {
+            _products.Add(new Product(name, price));
+        }
+
+        public void RemoveProduct(string productName)
+        {
+            Product targetProduct = _products.FirstOrDefault(x => x.Name == productName);
+            if (targetProduct != null)
+            {
+                _products.Remove(targetProduct);
+                return;
+            }
+            throw new InvalidOperationException();
+
+        }
+        public double GetTotalPrice()
+        {
+            double totalPrice = 0;
+            foreach (var item in _products)
+            {
+               totalPrice += item.Price;
+            }
+            return totalPrice;
+        }
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(_products);
+        }
 
     }
 }
